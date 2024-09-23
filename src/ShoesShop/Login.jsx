2ca46c539956237f99c3cfec56a1c206a8f45408
unknown <Aorus0804@gmail.com>
2024-09-23 10:56:06 +0700

    'version10'

--- src/ShoesShop/Login.jsx
+++ src/ShoesShop/Login.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
 import { NavLink, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import LoginFacebook from './LoginFacebook';
 
 const Login = () => {
   const navigate = useNavigate();
